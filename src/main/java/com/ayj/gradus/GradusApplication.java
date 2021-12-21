7463e34827cab04316e104f8e37ff3a5aca8f330
yak-fumblepack <liang.mike.to@gmail.com>
2021-12-21 10:42:04 -0500

    Initial commit

---
+++ src/main/java/com/ayj/gradus/GradusApplication.java
@@ -0,0 +1,31 @@
+/**
+ * Name: Mike Liang
+ * Date: December 21 2021
+ * Description:
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author @yak-fumblepack
+ */
+
+package com.ayj.gradus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This is the class of the application with the main method that runs the web
+ * app.
+ */
+@SpringBootApplication
+public class GradusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradusApplication.class, args);
+	}
+
+}
