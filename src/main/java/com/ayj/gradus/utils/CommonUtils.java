3359543f0b78c4abc8009716cd98e04005cd72db
yak-fumblepack <liang.mike.to@gmail.com>
2022-01-16 16:53:21 -0500

    update methods to help with autograding

--- src/main/java/com/ayj/gradus/utils/CommonUtils.java
+++ src/main/java/com/ayj/gradus/utils/CommonUtils.java
@@ -11,6 +11,10 @@ import org.springframework.web.servlet.ModelAndView;
  */
 public class CommonUtils {
 
+  private String os = System.getProperty("os.name").toLowerCase();
+
+  public String UPLOAD_DIR;
+
   /**
    * 
    * @return
@@ -34,6 +38,64 @@ public class CommonUtils {
     String userEmail = auth.getName();
 
     return userEmail;
+  }
+
+  /**
+   * 
+   * @return
+   */
+  public boolean isWindows() {
+    return (os.indexOf("win") >= 0);
+  }
+
+  /**
+   * 
+   * @return
+   */
+  public boolean isUNIX() {
+    return (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0 || os.indexOf("aix") > 0);
+  }
+
+  /**
+   * 
+   * @return
+   */
+  public String determineOS() {
+    boolean isWindows = isWindows();
+    boolean isUNIX = isUNIX();
+    if (isWindows) {
+      return "windows";
+    } else if (isUNIX) {
+      return "unix <3";
+    } else {
+      return "";
+    }
+  }
+
+  public void createOSPath() throws Exception {
+
+    String os = determineOS();
+
+    if (os.equals("windows")) {
+
+      this.UPLOAD_DIR = System.getProperty("user.home") + "\\Gradus\\uploads\\";
+
+    } else if (os.equals("unix <3")) {
+
+      this.UPLOAD_DIR = "~/Gradus/uploads";
+
+    } else {
+      throw new Exception("Unrecognised OS");
+    }
+
+  }
+
+  public String getUploadDir() throws Exception {
+
+    createOSPath();
+
+    return this.UPLOAD_DIR;
+
   }
 
 }
